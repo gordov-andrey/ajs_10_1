3ddd31c632cf538fac1fd98dcad1f1654ad0ea42
“Andrey <gordov.ar@gmail.com>
2021-12-17 13:30:48 +0500

    1.0.0

--- src/js/__tests__/GamingSavingLoader.test.js
+++ src/js/__tests__/GamingSavingLoader.test.js
@@ -4,7 +4,7 @@ import GameSaving from '../GameSaving';
 const data = '{"id": 9,"created": 1546300800, "userInfo": {"id": 1, "name": "Hitman", "level": 10, "points":2000}}';
 const hitman = new GameSaving(JSON.parse(data));
 
-test('Test case №1', (done) => {
+test('Test', (done) => {
   GameSavingLoader.load().then((result) => {
     expect(result).toEqual(hitman);
   }).catch((err) => err);
